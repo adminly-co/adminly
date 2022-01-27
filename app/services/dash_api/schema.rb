cdd88f100860fed2e44144e248fa03be8fc747c1
Scott Nelson <scott@scottnelson.co>
2022-01-27 12:32:34 -0500

    Include enum values in schema

--- app/services/dash_api/schema.rb
+++ app/services/dash_api/schema.rb
@@ -1,39 +1,40 @@
 module DashApi
-  module Schema 
+  module Schema
 
     EXCLUDED_TABLES = [
-      'ar_internal_metadata', 
+      'ar_internal_metadata',
       'schema_migrations'
     ]
 
-    attr_accessor :tables 
+    attr_accessor :tables
 
     def self.table_names
       @tables = ActiveRecord::Base.connection.tables
-      @tables.filter!{|t| !EXCLUDED_TABLES.include?(t)}.sort!
-    end 
+      @tables.filter!{|t| !EXCLUDED_TABLES.include?(t)}
+      @tables.sort!
+    end
 
-    def self.table_schema(table_name) 
-      @tables = table_names 
+    def self.table_schema(table_name)
+      @tables = table_names
       dash_table = DashTable.modelize(table_name)
-      dash_table.reset_column_information      
-      dash_table.columns.map{ |column| 
+      dash_table.reset_column_information
+      dash_table.columns.map{ |column|
         render_column(column)
-      } 
-    end 
+      }
+    end
 
-    def self.db_schema 
+    def self.db_schema
       @tables = table_names
       schema = {
         tables: @tables
       }
       @tables.each do |table_name|
         schema[table_name] = table_schema(table_name)
-      end      
-      schema   
-    end 
+      end
+      schema
+    end
 
-    def self.render_column(column)      
+    def self.render_column(column)
       {
         friendly_name: column.human_name,
         name: column.name,
@@ -43,18 +44,30 @@ module DashApi
         limit: column.sql_type_metadata.limit,
         precision: column.sql_type_metadata.precision,
         foreign_key: foreign_key?(column.name),
-        foreign_table: foreign_table(column.name) 
+        foreign_table: foreign_table(column.name),
+        enum_values: enum_values(column),
       }
-    end 
+    end
 
-    def self.foreign_key?(column_name)      
+    def self.foreign_key?(column_name)
       column_name[-2..-1]&.downcase === 'id' && column_name.downcase != 'id'
-    end 
+    end
 
     def self.foreign_table(column_name)
-      table_prefix = column_name[0...-3]      
+      table_prefix = column_name[0...-3]
       @tables.find{|t| t === table_prefix || t === table_prefix.pluralize }
-    end 
-    
-  end 
-end 
+    end
+
+    def self.enum_values(column)
+      return nil if column.type != :enum
+
+      conn = ActiveRecord::Base.connection
+      oid = conn.quote(column.sql_type_metadata.oid)
+
+      conn
+        .execute("SELECT enumlabel FROM pg_enum WHERE enumtypid = #{oid} ORDER BY enumsortorder")
+        .pluck('enumlabel')
+    end
+
+  end
+end
