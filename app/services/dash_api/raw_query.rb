dff49b3cfe04315c68848747c2c6e18a48e3a0ce
Scott Nelson <scott@scottnelson.co>
2022-01-28 14:07:19 -0500

    Added raw SQL query endpoint

---
+++ app/services/dash_api/raw_query.rb
@@ -0,0 +1,24 @@
+module DashApi
+  module RawQuery
+    class QueryError < StandardError; end
+
+    def self.execute(sql)
+      sql = sanitize(sql)
+      ActiveRecord::Base.connection.select_all(sql)
+    end
+
+    def self.sanitize(sql)
+      tokens = sql.split(/\s/)
+
+      # only allow select statements
+      if tokens[0].downcase != 'select'
+        raise QueryError
+      end
+
+      # remove semicolons to prevent the chaining of multiple statements
+      sql = sql.gsub(';', '')
+
+      sql
+    end
+  end
+end
