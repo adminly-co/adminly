23e667cb37127cd0cd3f6f365b2052a1a70895a9
Scott Nelson <scott@scottnelson.co>
2022-03-09 15:43:00 -0500

    Dynamic filter values

--- app/services/adminly/query_params.rb
+++ app/services/adminly/query_params.rb
@@ -38,6 +38,16 @@ module Adminly
       count: "count"
     }
 
+    DYNAMIC_FILTER_VALUES = {
+      '1_day_ago' => -> { 1.day.ago },
+      '7_days_ago' => -> { 7.days.ago },
+      '14_days_ago' => -> { 14.days.ago },
+      '30_days_ago' => -> { 30.days.ago },
+      '60_days_ago' => -> { 60.days.ago },
+      '90_days_ago' => -> { 90.days.ago },
+      'current_year' => -> { Time.current.year },
+    }
+
     # QueryParams is a ruby Class which parses URL parameters
     # passed to a Rails Controller into attributes used to query models
     attr_accessor :params
@@ -165,7 +175,9 @@ module Adminly
       rel = "eq" unless OPERATORS.keys.include?(rel.to_sym)
       operator = OPERATORS[rel.to_sym] || '='
 
-      if value =~ DATE_REGEX
+      if DYNAMIC_FILTER_VALUES.has_key?(value)
+        value = DYNAMIC_FILTER_VALUES[value].()
+      elsif value =~ DATE_REGEX
         value = DateTime.parse(value)
       elsif BOOLEANS.include?(value&.downcase)
         value = true if value.downcase === "true"
